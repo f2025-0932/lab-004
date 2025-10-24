71d28b0750152a1e2fe0ae841f1c6fed528d63a1
f2025-0932 <f2025-0932@bnu.edu.pk>
2025-10-24 14:57:02 +0500

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+cout<<"Name:\tMeerab fatima\n";
+cout<<"Department:\tScit\n";
+cout<<"Qoute\t\"knowledge is power\"\n";
+
+    return 0;
+}
